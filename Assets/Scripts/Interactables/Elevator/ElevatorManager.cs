3994e9e91b6caae8bba3238e6c58005212045b9b
everulicess <ulices.vaca12@gmail.com>
2023-12-06 14:32:32 +0100

    elevator anim try 2

---
+++ Assets/Scripts/Interactables/Elevator/ElevatorManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorManager : MonoBehaviour
+{
+    [SerializeField] Animator doorAnim;
+    [SerializeField] Animator elevatorAnim;
+
+    public void OnGroundFloorButtonPressed()
+    {
+        PlayAnimation(doorAnim,"DoorOpen");
+        PlayAnimation(elevatorAnim, "ElevatorDown");
+        PlayAnimation(doorAnim, "DoorClosed");
+    }
+    public void OnFirstFloorButtonPressed()
+    {
+        PlayAnimation(doorAnim, "DoorOpen");
+        PlayAnimation(elevatorAnim, "ElevatorUp");
+        PlayAnimation(doorAnim, "DoorClosed");
+    }
+    public void GoToFirstFloor()
+    {
+        PlayAnimation(doorAnim, "DoorClosed");
+        PlayAnimation(elevatorAnim, "ElevatorDown");
+        PlayAnimation(doorAnim, "DoorOpen");
+    }
+    public void GoToGroundFloor()
+    {
+        PlayAnimation(doorAnim, "DoorClosed");
+        PlayAnimation(elevatorAnim, "ElevatorUp");
+        PlayAnimation(doorAnim, "DoorOpen");
+    }
+    private void PlayAnimation(Animator anim, string animationName)
+    {
+        anim.Play(animationName);
+    }
+}
