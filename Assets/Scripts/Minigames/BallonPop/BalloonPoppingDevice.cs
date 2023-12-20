a60f11e125b84dff4c165ceb068bffa3f9af901a
everulicess <ulices.vaca12@gmail.com>
2023-12-20 07:59:36 +0100

    Balloon Popping Device

---
+++ Assets/Scripts/Minigames/BallonPop/BalloonPoppingDevice.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class BalloonPoppingDevice : MonoBehaviour
+{
+    [SerializeField] GameObject bullet;
+    public Transform spawnPoint;
+    public float fireSpeed = 20f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        XRGrabInteractable grabbable = GetComponent<XRGrabInteractable>();
+        grabbable.activated.AddListener(FireBullet);
+    }
+
+    int timesFired = 4;
+
+    public void FireBullet(ActivateEventArgs arg)
+    {
+        if (timesFired == 0) return;
+
+        timesFired--;
+        GameObject spawnObject = Instantiate(bullet);
+        spawnObject.transform.position = spawnPoint.position;
+        spawnObject.GetComponent<Rigidbody>().velocity = spawnPoint.forward * fireSpeed;
+        Destroy(spawnObject, 3f);
+    }
+        
+    
+}
