333969d246c2e4f1a39827f91fd9903cbb860c14
everulicess <ulices.vaca12@gmail.com>
2023-12-26 22:48:33 +0100

    UI placeholders and switch statement for the game stages

--- Assets/Scripts/Minigames/BallonPop/BalloonManager.cs
+++ Assets/Scripts/Minigames/BallonPop/BalloonManager.cs
@@ -7,45 +7,55 @@ public class BalloonManager : MonoBehaviour
 {
     private List<string> balloons = new List<string>();
     private List<string> textListToCheck = new List<string>();
+    private List<GameObject> Darts = new List<GameObject>();
 
-    [SerializeField] GameObject balloonPrefab;
     [SerializeField] TextMeshProUGUI postDescription;
 
     [Header("Minigame Settings")]
     public string[] balloonText;
-    public int balloonsAmount;
-    
-    [HideInInspector] public int score;
 
+    //game flow variables
+    [SerializeField] private bool isExplanationDone = false;
+    [SerializeField] GameObject gameExplanationObject;
+    [SerializeField] private int balloonsCount;
+    [SerializeField] GameObject resultObject;
+    private GameState gameState;
 
     private void Awake()
     {
+        gameState = GameState.Explanation;
         PutTextOnTheList();
+        foreach (GameObject dart in GameObject.FindGameObjectsWithTag("Dart"))
+        {
+            Darts.Add(dart);
+        }
         DisplayResultText("");
+    }
+    private void Update()
+    {
+        HandleMinigameFlow();
     }
 
     public void GetText(Balloon balloon)
     {
-        //Debug.Log(balloons.Count);
-        if (balloons.Count > 0)
-        {
-            int stringPos = Random.Range(0, balloons.Count - 1);
-            balloon.balloonText.text = balloons[stringPos];
-            balloons.RemoveAt(stringPos);
-        }   
+        if (balloons.Count <= 0) return;
+        
+        int stringPos = Random.Range(0, balloons.Count - 1);
+        balloon.balloonText.text = balloons[stringPos];
+        balloons.RemoveAt(stringPos);
     }
     public void DisplayResultText(string _balloonText)
     {
         for (int i = 0; i < balloonText.Length; i++)
         {
-            if (string.Equals(_balloonText, balloonText[i]))
-            {
-                textListToCheck.Remove(balloonText[i]);      
-            }
+            if (!string.Equals(_balloonText, balloonText[i])) return;
+            
+            textListToCheck.Remove(balloonText[i]);      
+            
             postDescription.text = $" ";
             foreach (string str in textListToCheck)
             {
-                postDescription.text += $"{str} ";
+                postDescription.text += $"{str}\n ";
             }
         } Debug.Log(textListToCheck.Count);
     }
@@ -56,6 +66,40 @@ public class BalloonManager : MonoBehaviour
             balloons.Add(str);
 
             textListToCheck.Add(str);
+        }
+    }
+    public void OnStartClicked()
+    {
+        isExplanationDone = true;
+    }
+    enum GameState
+    {
+        Explanation,
+        PlayingTheGame,
+        ShowingTheResult,
+        Finished,
+    }
+    private void HandleMinigameFlow()
+    {
+        Debug.Log($"game state: {gameState}");
+        switch (gameState)
+        {
+            case GameState.Explanation:
+                gameExplanationObject.SetActive(true);
+                resultObject.SetActive(false);
+                if (!isExplanationDone) return;
+                gameExplanationObject.SetActive(false);
+                gameState = GameState.PlayingTheGame;
+                break;
+            case GameState.PlayingTheGame:
+                if (balloonsCount > 0) return;
+                gameState = GameState.ShowingTheResult;
+                break;
+            case GameState.ShowingTheResult:
+                resultObject.SetActive(true);
+                break;
+            default:
+                break;
         }
     }
 }
